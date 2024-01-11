a4d73fe8a1dfbe60f2800a9a785425b2debadeab
Danil Tolkachev <tolkachev@nkbvs.ttn.ru>
2024-01-11 17:25:21 +0300

    planeta2 sources

---
+++ keyboards/ergohaven/planeta2/rules.mk
@@ -0,0 +1,14 @@
+SERIAL_DRIVER = vendor
+
+OPT_DEFS = -O2
+QUANTUM_PAINTER_ENABLE = yes
+QUANTUM_PAINTER_DRIVERS += st7789_spi
+QUANTUM_PAINTER_LVGL_INTEGRATION = yes
+
+SRC += lvgl_helpers.c \
+	   display.c
+
+# Build Options
+RGBLIGHT_ENABLE = yes
+AUDIO_ENABLE = yes
+AUDIO_DRIVER = pwm_hardware
